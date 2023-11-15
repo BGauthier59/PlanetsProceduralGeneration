cf57bf3895b7bbfdaec333b47675eba6d1e746e8
BGauthier59 <gauthierbaudinet@gmail.com>
2023-11-15 13:05:27 +0100

    controller v1

---
+++ IcosphereVillage/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform currentPlanet;
+
+    // Rotation
+    private Vector2 mouseMove;
+    private bool isDraggingPlanet;
+    [SerializeField] private float rotateSensibility;
+
+    // Zoom
+    [SerializeField] private Transform camera;
+    [SerializeField] private float baseDistance;
+    [SerializeField] private Vector2 minMaxDistance;
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private float stepValue;
+    private float currentDistance;
+    private Vector2 scroll;
+
+    private void Start()
+    {
+        currentDistance = baseDistance;
+    }
+
+    public void OnRotate(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.performed) return;
+        mouseMove = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnDragPlanet(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started) isDraggingPlanet = true;
+        else if (ctx.canceled) isDraggingPlanet = false;
+    }
+
+    public void OnScroll(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.performed) return;
+        scroll = ctx.ReadValue<Vector2>();
+        SetDistance(-scroll.y);
+    }
+
+    private void Update()
+    {
+        if (isDraggingPlanet) RotateDraggedPlanet();
+        //RotateDraggedPlanet();
+    }
+
+    private void LateUpdate()
+    {
+        Zoom();
+    }
+
+    private void RotateDraggedPlanet()
+    {
+        currentPlanet.RotateAround(Vector3.up, math.radians(-mouseMove.x * rotateSensibility));
+        currentPlanet.RotateAround(Vector3.right, math.radians(mouseMove.y * rotateSensibility));
+    }
+
+    private void SetDistance(float delta)
+    {
+        currentDistance += delta * stepValue;
+        currentDistance = math.clamp(currentDistance, minMaxDistance.x, minMaxDistance.y);
+    }
+
+    private void Zoom()
+    {
+        Vector3 targetPos = -Vector3.forward * baseDistance - camera.forward * currentDistance;
+        camera.position = Vector3.Lerp(camera.position, targetPos, cameraSpeed * Time.deltaTime);
+    }
+}
